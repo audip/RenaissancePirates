edec126562f97afdd38913c6298123a33bb80418
Aditya Purandare <aditya.p1993@hotmail.com>
2014-02-18 21:58:15 +0530

    Video question just added

---
+++ video.php
@@ -0,0 +1,56 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <style type="text/css">
+    body{
+                font-size: 15px;
+                font-family: Montserrat, Helvetica Neue, sans-serif;
+                margin-top: 30px;
+                margin-left: 30px;
+                width: 100%;
+                color: white;
+                background-color: #8e44ad;
+            }
+            .underline{
+                text-decoration: underline;
+            }
+            h2{
+                text-align: center;
+                font-style: italic;
+            }
+            #code, pre{
+	            margin: 50px auto;
+            }
+            .vid, body{
+	            margin: 10px 30px 10px 30px;
+	            width: 95%;
+            }
+            .vid{
+	            text-align: center;
+            }
+    </style>
+
+    <title></title>
+</head>
+
+<body>
+    <h2>Inspirational Video Question</h2>
+
+    <p>'The Journey of Biggest Failure in College', Bill Gates talks about His experience which will motivate you to be a Better Graduate(Engineer), Here you can learn from his experience when to seek and opportunity and How you should not let it go by Seizing it when there is time. We want you to be a better engineer and it's our sincere effort to guide you in the Right Direction</p>
+    
+    <p>Pirate, you must watch this video and Answer the Question at the bottom of this page. Hit Subscribe to Our Eclectika Channel(Optional)</p>
+	<div class="vid">
+	<iframe width="70%" height="80%" src="//www.youtube.com/embed/iADTpgRXYrk" frameborder="0" allowfullscreen></iframe>
+
+<p><span class="underline">Ques.</span> What all did Bill Gates learn at Harvard?</p>
+<p><span class="underline">Ans.</span>New ideas in <input type="text"  size="20" maxlength="20" name="clue1" required="required"/> and <input type="text"  size="20" maxlength="20" name="clue2" required="required"/></p>
+
+</div>
+
+</body>
+</html>
+<?php include('foot.php'); ?>
